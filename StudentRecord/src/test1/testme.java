8575cb6acccbec609dbb95075052da338642df58
hardy91 <hardeepsng@gmail.com>
2014-09-20 09:02:54 -0700

    no wala

--- StudentRecord/src/test1/testme.java
+++ StudentRecord/src/test1/testme.java
@@ -5,7 +5,7 @@ public class testme {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("first commit");
-System.out.println("Second commit yes");
+System.out.println("Second commit no");
 	}
 
 }
